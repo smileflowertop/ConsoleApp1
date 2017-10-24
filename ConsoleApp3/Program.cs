5ec7730ef94cf2fe2a2a694de53f7c14b36ec46d
Administrator <Administrator@ZHANGSAI123-PC>
2017-10-24 11:25:41 +0800

    123

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student(105);
+            Console.Write(stu.Age);
+            stu.Age = 100;
+            Console.Write(stu.Age);
+        }
+    }
+    class Student
+    {
+        private int _age = 10;
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                if (value > 0 && value <= 100)
+                {
+                    this._age = value;
+                }
+            }
+        }
+        public Student(int age)
+        {
+            this._age = age;
+        }
+    }
+}
